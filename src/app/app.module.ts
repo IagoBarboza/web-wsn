f101c7927d657345f2b7a69a104036944556f128
Iago Barboza <iagobarboza@gmail.com>
2017-03-27 10:07:26 -0300

    + Single app with 2 modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NodeModule } from './node/node.module';
 import { SinkModule } from './sink/sink.module';
+
+import { NodeComponent } from './node/node.component';
+import { SinkComponent } from './sink/sink.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyC0QNh31XmC_iLKd_tje3H53rgmrLNYiV4",
@@ -23,6 +28,11 @@ export const firebaseConfig = {
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot([
+      { path: 'node', component: NodeComponent },
+      { path: 'sink', component: SinkComponent }
+    ]),
+    NodeModule,
     SinkModule
   ],
   providers: [],
